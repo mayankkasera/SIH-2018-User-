58c58cf867492a71c613a0afd394036dc1b8fcbe
mayankkasera <mayankkasera98@gmail.com>
2018-03-15 04:07:04 +0530

    This is first commit by me

--- app/src/main/java/dynamicdrillers/sih2018user/HomeFragment.java
+++ app/src/main/java/dynamicdrillers/sih2018user/HomeFragment.java
@@ -1,16 +1,19 @@
 package dynamicdrillers.sih2018user;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class HomeFragment extends Fragment {
+
+    Button button;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -20,9 +23,18 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
         // Inflate the layout for this fragment
-        /////
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+        button = view.findViewById(R.id.register_home_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getContext(),MapsActivity.class));
+            }
+        });
+        return view;
     }
 
    
