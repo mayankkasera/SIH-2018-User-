4f1f06fcf3c1c96d186bbb5ef4136e529de554c9
Happy Singh <happysingh23828@gmail.com>
2018-03-14 16:00:13 +0530

    otp verification layout done

--- app/src/main/java/dynamicdrillers/sih2018user/MainActivity.java
+++ app/src/main/java/dynamicdrillers/sih2018user/MainActivity.java
@@ -1,13 +1,22 @@
 package dynamicdrillers.sih2018user;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
